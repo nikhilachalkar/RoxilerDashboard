97215d51b2f1088eb09ae0a42b4277f914aa7313
Nikhil Achalkar <142605075+nikhilachalkar@users.noreply.github.com>
2024-10-12 11:49:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,30 +16,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-// // Example API to filter by month
-// app.get('/products-by-month', async (req, res) => {
-//   const { month } = req.query; // expected value is a number between 1 (January) and 12 (December)
-  
-//   if (!month || isNaN(month) || month < 1 || month > 12) {
-//     return res.status(400).send('Invalid month');
-//   }
-
-//   try {
-//     const collection = db.collection('products');
-    
-//     const products = await collection.find({
-//       dateOfSale: {
-//         $gte: new Date(2023, month - 1, 1),
-//         $lt: new Date(2023, month, 1)
-//       }
-//     }).toArray();
-
-//     res.json(products);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send('Error retrieving products');
-//   }
-// });
 
 const startServer = async () => {
   await connectDB();
