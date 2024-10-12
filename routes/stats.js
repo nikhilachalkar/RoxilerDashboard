6f79642f878057d1dc2a1f9d53bf43720396841a
Nikhil Achalkar <142605075+nikhilachalkar@users.noreply.github.com>
2024-10-12 11:50:59 +0530

    Update stats.js

--- routes/stats.js
+++ routes/stats.js
@@ -1,10 +1,9 @@
-// routes/statistics.js
 const express = require('express');
 const { getStatistics } = require('../controllers/statController');
 
 const router = express.Router();
 
-// Route to get statistics
+
 router.get('/statistics', getStatistics);
 
 module.exports = router;
