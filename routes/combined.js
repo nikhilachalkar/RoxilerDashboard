fb64a30a229f415c735c24bc8452da5b9bcbb5f0
Nikhil Achalkar <142605075+nikhilachalkar@users.noreply.github.com>
2024-10-12 11:50:38 +0530

    Update combined.js

--- routes/combined.js
+++ routes/combined.js
@@ -1,10 +1,10 @@
-// routes/combined.js
+
 const express = require('express');
 const { getCombinedData } = require('../controllers/combinedController');
 
 const router = express.Router();
 
-// Route to get combined data
+
 router.get('/combined-data', getCombinedData);
 
 module.exports = router;
